c37c0216bacfa1e48df7a658b65f0bc46dafbb29
sanchi0204 <sanchirastogi02@gmail.com>
2020-12-11 12:16:48 +0530

    Create Distribute Candies.cpp

---
+++ Distribute Candies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+
+        unordered_set<int> s;
+
+
+        for(int i=0;i<candyType.size();i++)
+        {
+            s.insert(candyType[i]);
+        }
+
+        if(s.size()>candyType.size()/2)
+            return candyType.size()/2;
+
+    return s.size();
+    }
+};
