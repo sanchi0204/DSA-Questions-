7dc8696b72f82029322712e705174324f56bee1f
sanchi0204 <sanchirastogi02@gmail.com>
2021-05-05 11:37:49 +0530

    Create Count pairs with given sum .cpp

---
+++ GeeksForGeeks/Count pairs with given sum .cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+
+        unordered_map<int,int> m;
+        int res=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(m.find(k-arr[i])!=m.end())
+                res+=m[k-arr[i]];
+
+            m[arr[i]]++;
+        }
+
+        return res;
+    }
+};
