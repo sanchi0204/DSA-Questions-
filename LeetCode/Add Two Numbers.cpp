8a4a1d49d4e7cd6cdbdf74733c4718f829a2a1ea
sanchi0204 <sanchirastogi02@gmail.com>
2021-06-16 20:05:32 +0530

    Create Add Two Numbers.cpp

---
+++ LeetCode/Add Two Numbers.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        ListNode* curr = new ListNode();
+        ListNode* res = curr;
+        int carry=0;
+
+
+        while(l1 || l2 || carry>0)
+        {
+            int sum=0;
+            if(l1)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+
+            if(l2)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+
+            sum+=carry;
+
+
+            ListNode* node = new ListNode(sum%10);
+              carry = sum/10;
+            curr->next=node;
+            curr=curr->next;
+
+        }
+
+
+        return res->next;
+
+    }
+};
