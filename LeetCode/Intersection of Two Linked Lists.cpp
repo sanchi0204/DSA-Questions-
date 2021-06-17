d0a80c1b49578a62173f238fa30b412486e70f3f
sanchi0204 <sanchirastogi02@gmail.com>
2021-06-18 00:10:43 +0530

    Create Intersection of Two Linked Lists.cpp

---
+++ LeetCode/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        if(headA==NULL || headB==NULL)
+            return NULL;
+
+        ListNode *a= headA;
+        ListNode *b = headB;
+
+        while(a!=b)
+        {
+            a=a==NULL?a=headB:a=a->next;
+            b=b==NULL?b=headA:b=b->next;
+        }
+
+        return a;
+    }
+};
