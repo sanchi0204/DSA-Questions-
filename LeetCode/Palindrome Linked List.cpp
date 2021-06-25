9684894d1a95ca143dd22cf9598d7b8b250cbb3c
sanchi0204 <sanchirastogi02@gmail.com>
2021-06-25 10:51:16 +0530

    Create Palindrome Linked List.cpp

---
+++ LeetCode/Palindrome Linked List.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverseList(ListNode* head)
+    {
+        // if(head==NULL || head->next==NULL)
+        //     return NULL;
+
+        ListNode *prev=NULL;
+        ListNode *next=NULL;
+        ListNode *curr=head;
+
+        while(curr)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+
+        if(head==NULL || head->next==NULL)
+            return true;
+
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        while(fast->next && fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        ListNode *rev = reverseList(slow->next);
+
+        ListNode *curr=head;
+
+        while(rev)
+        {
+            if(rev->val!=curr->val)
+                return false;
+
+            curr=curr->next;
+            rev=rev->next;
+
+        }
+
+        return true;
+
+    }
+};
