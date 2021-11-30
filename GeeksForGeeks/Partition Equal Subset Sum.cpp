7718ea93523e9afefdba21eb970b10708d6c8811
sanchi0204 <sanchirastogi02@gmail.com>
2021-11-30 22:49:39 +0530

    Create Partition Equal Subset Sum.cpp

---
+++ GeeksForGeeks/Partition Equal Subset Sum.cpp
@@ -0,0 +1,54 @@
+class Solution{
+public:
+
+    bool SubsetSum(int N, int arr[], int sum)
+    {
+        bool t[N+1][sum+1];
+
+        for(int i=0;i<N+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                    t[i][j]=false;
+
+                if(j==0)
+                    t[i][j]=true;
+            }
+        }
+
+        for(int i=1;i<N+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+
+        return t[N][sum];
+    }
+
+    int equalPartition(int N, int arr[])
+    {
+
+        int sum=0;
+        for(int i=0;i<N;i++)
+            sum+=arr[i];
+
+        if(sum%2!=0)
+            return 0;
+
+        else
+        {
+        if(SubsetSum(N,arr,sum/2))
+            return 1;
+
+        else
+            return 0;
+        }
+    }
+};
