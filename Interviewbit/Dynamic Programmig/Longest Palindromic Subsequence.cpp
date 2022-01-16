21b1ea81deb171e988ff2816468c4d75c409a93e
sanchi0204 <sanchirastogi02@gmail.com>
2022-01-16 22:35:25 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Interviewbit/Dynamic Programmig/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,34 @@
+int LCS(string A, string B)
+{
+        int n=A.length();
+            int t[n+1][n+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                    t[i][j]=0;
+            }
+        }
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(A[i-1]==B[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+
+                else
+                    t[i][j]=max(t[i-1][j], t[i][j-1]);
+
+            }
+        }
+
+     return t[n][n];
+}
+int Solution::solve(string A) {
+
+    string rev= string(A.rbegin(), A.rend());
+    return LCS(A,rev);
+}
