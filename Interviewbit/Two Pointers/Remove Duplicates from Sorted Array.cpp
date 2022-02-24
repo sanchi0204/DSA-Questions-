d5f1f4ee9691b25e60a20b09e8df5fc468f78fda
sanchi0204 <sanchirastogi02@gmail.com>
2022-02-24 12:01:44 +0530

    Create Remove Duplicates from Sorted Array.cpp

---
+++ Interviewbit/Two Pointers/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,18 @@
+int Solution::removeDuplicates(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int res = 1;
+    for(int i=1;i<A.size();i++)
+    {
+        if(A[i]!=A[res-1])
+                {
+                    A[res]=A[i];
+                    res++;
+                }
+    }
+return res;
+
+}
